dbec4c2f25c155e46554f52d7dfeaa6f6ed7e848
MASRAFE BIN HANNAN SIAM <masrafesiammbhs1633@gmail.com>
2022-05-24 18:18:35 +0600

    Practice

---
+++ Practice/Practice 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int fav_num;
+    cout<< "Enter the num :";
+    cin>> fav_num;
+    cout<< "That's great"<<endl;
+    return 0;
+}
