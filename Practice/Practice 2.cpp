8f3e91b6a45137e42b67bc2197ecc9c80e9c58a1
MASRAFE BIN HANNAN SIAM <masrafesiammbhs1633@gmail.com>
2022-05-24 19:13:58 +0600

    Practice

---
+++ Practice/Practice 2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int width,length;
+cout<<"Enther width:";
+cin>>width;
+cout<<"Enter length :";
+cin>>length;
+cout<<"Area is " << width*length<<" sqfeet"<<endl;
+    return 0;
+}
